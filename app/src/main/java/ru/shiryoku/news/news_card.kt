c21b32830596bd29d6fd0323fe2a4220cb129ce3
sh1ryoku <sparkypx@gmail.com>
2023-07-20 22:11:46 +0700

    Сверстал карточку новости

---
+++ app/src/main/java/ru/shiryoku/news/news_card.kt
@@ -0,0 +1,16 @@
+package ru.shiryoku.news
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class news_card : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
