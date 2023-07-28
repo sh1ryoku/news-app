aaa154ca0adf7a375098b4fb75557556098ce0f1
sh1ryoku <sparkypx@gmail.com>
2023-07-29 01:41:31 +0700

    удалил неиспользуемые функции, поменял стили

--- app/src/main/java/ru/shiryoku/news/extension/int/metricsConversion.kt
+++
@@ -1,6 +0,0 @@
-package ru.shiryoku.news.extension.int
-
-import android.content.res.Resources
-
-fun Int.toDp(): Int = (this / Resources.getSystem().displayMetrics.density).toInt()
-fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
